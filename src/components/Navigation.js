eb597c584374ea8b81984092cc6c6e5c4f26e1cd
hyejooLim <71072930+hyejooLim@users.noreply.github.com>
2021-01-31 22:54:17 +0900

    components

---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+
+function Navigation() {
+  return (
+    <div className="nav">
+      <div className="home">
+        <Link to="/">Home</Link>
+      </div>
+      <div className="about">
+        <Link to="/about">About</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
