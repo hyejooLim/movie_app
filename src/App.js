34b67e1b063a9046077b90e9570cb9cd89b23544
hyejooLim <71072930+hyejooLim@users.noreply.github.com>
2021-01-31 13:35:03 +0900

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
+import "./Movie.css"
+class App extends React.Component {
+  // App 클래스는 react component를 상속받음
+  state = {
+    isLoading: true,
+    movies: [],
+  };
 
-function App() {
-  return (
-    <div>
-      <h1>Hello!!!!</h1>
-    </div> 
-  );
+  async componentDidMount() {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies: movies, isLoading: false });
+  }
+
+  render() {
+    const { movies, isLoading } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
